273d1c7c2174d1f6dd1800f8c8b2b798e4e3a90d
Joakim Reinert <mail@jreinert.com>
2013-05-23 13:41:22 +0200

    added line to only poll and not download

--- pacman/poll-updates.sh
+++ pacman/poll-updates.sh
@@ -7,6 +7,11 @@ user="your username"
 # Download packages needing an update
 nice -n 50 pacman -Syuwq --noconfirm > /dev/null
 
+# Comment out/remove the line above and uncomment the one
+# below to only poll updates without downloading them
+
+# pacman -Sy > /dev/null
+
 # Write the packages needing an update to file
 pacman -Quq | tr "\\n" " " | xargs expac -S '%r/%n [%v]' > /home/$user/.pacmanupdates
 
